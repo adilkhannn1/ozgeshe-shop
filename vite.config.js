526f2318fd80dfd50fb78a1d7f9b13f6b3592292
adilkhannn1 <adilkhanbala04@gmail.com>
2025-04-26 13:35:37 +0500

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FullstackEcommerceApp/',
+  base: '/ozgeshe-shop/',
   plugins: [
     tailwindcss(),
     vue(),
